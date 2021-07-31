3046962fc066b80c3e88b59dc2a227c8c167e042
Shameera17 <shameera.carrim17@gmail.com>
2021-07-31 11:19:56 +0530

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
+// Node + Express -> server setup
+
 import express , {Response, Request} from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+require('dotenv').config()
+
+// init express app
 const app = express();
+
+// import routes
+
+// connect the MongoDb
+const uri : any  = process.env.URI
+mongoose.connect(uri, {
+useCreateIndex : true,
+useFindAndModify: false,
+useUnifiedTopology: true,
+useNewUrlParser: true
+}).then(data => console.log("Database connected successfully"))
+.catch(err => console.log("Database connection unsuccessful"))
+
+// config middlewares
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+// config route middlewares
+
+// server set up
 
 app.get('/', (req : Request , res: Response)=>{
     const age : number = 39;
     res.json({message: "please like the video"})
 })
 
-app.listen('3000', () : void => {
-    console.log("server is running")
+const port = process.env.PORT
+app.listen(port, () : void => {
+    if(!port) console.log("server is not running")
+    console.log(`server is running @ http://localhost:${port}`)
 })
